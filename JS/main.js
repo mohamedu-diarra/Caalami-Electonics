2cfd217d9422e8cea899b0fa0669713bf5ad685c
Mohamed Abdirahman <mohamedepdirahman@gmail.com>
2025-10-25 05:42:20 +0300

    added shopnow js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,11 @@
 const btncart = document.querySelector("button.cart");
 const btnsig = document.querySelector("button.signup");
+const shop = document.querySelector("button.shop")
 btnsig.addEventListener("click",()=>{
      
+})
+
+
+shop.addEventListener("click", ()=>{
+    window.location.href = "signup.html";
 })
